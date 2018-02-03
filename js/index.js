2728ab0ee5558db3885ef55db6cd67f0f30925bb
nanter1986 <nanter1986@gmail.com>
2018-02-03 11:07:43 +0200

    first draft

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
 
+  var powerRankings=[];
+
+  for(let i=0;i<players.length;i++){
+    var pr=0.5*(2*players[i].winsTotal-players[i].losesTotal)+0.5*(2*players[i].winsWeek-players[i].losesWeek)
+    console.log(players[i].name+":"+pr);
+    var currentPlayer={};
+    currentPlayer.name=players[i].name;
+    currentPlayer.image=players[i].image;
+    currentPlayer.score=pr;
+    console.log(currentPlayer);
+    powerRankings.push(currentPlayer);
+
+  }
+
+
+
     $(".nav li").on("click", function () {
         console.log("clicked on item");
         $(".nav li").removeClass("active");
---
+++ js/players.js
@@ -0,0 +1,222 @@
+const players=[
+  {
+    name:"Γκότσης Ηλίας",
+    image:"",
+    winsTotal:10,
+    losesTotal:2,
+    winsWeek:4,
+    losesWeek:2
+  },
+  {
+    name:"Τουρόβνικ Ντάρια",
+    image:"",
+    winsTotal:11,
+    losesTotal:5,
+    winsWeek:6,
+    losesWeek:1
+  },
+  {
+    name:"Δαλάκα Αικατερίνη",
+    image:"",
+    winsTotal:12,
+    losesTotal:6,
+    winsWeek:4,
+    losesWeek:2
+  },
+  {
+    name:"Αγόρου Νικόλας",
+    image:"",
+    winsTotal:10,
+    losesTotal:6,
+    winsWeek:3,
+    losesWeek:3
+  },
+  {
+    name:"Ανδρονικίδου Ζωή",
+    image:"",
+    winsTotal:5,
+    losesTotal:3,
+    winsWeek:0,
+    losesWeek:0
+  },
+  {
+    name:"Μεταξά Μελίνα",
+    image:"",
+    winsTotal:10,
+    losesTotal:7,
+    winsWeek:3,
+    losesWeek:3
+  },
+  {
+    name:"Γιακουμάτος Χάρης",
+    image:"",
+    winsTotal:9,
+    losesTotal:7,
+    winsWeek:5,
+    losesWeek:3
+  },
+  {
+    name:"Κρητικός Στέλιος",
+    image:"",
+    winsTotal:8,
+    losesTotal:8,
+    winsWeek:7,
+    losesWeek:2
+  },
+  {
+    name:"Τσίλης Γιάννης",
+    image:"",
+    winsTotal:8,
+    losesTotal:9,
+    winsWeek:4,
+    losesWeek:3
+  },
+  {
+    name:"Φαρμάκη Όλγα",
+    image:"",
+    winsTotal:7,
+    losesTotal:6,
+    winsWeek:2,
+    losesWeek:4
+  },
+  {
+    name:"Σαμαρινού Μαρία",
+    image:"",
+    winsTotal:7,
+    losesTotal:7,
+    winsWeek:1,
+    losesWeek:5
+  },
+  {
+    name:"Μουρούτσος Μιχάλης",
+    image:"",
+    winsTotal:7,
+    losesTotal:7,
+    winsWeek:4,
+    losesWeek:3
+  },
+  {
+    name:"Δικαιούλια Βιργινία",
+    image:"",
+    winsTotal:7,
+    losesTotal:8,
+    winsWeek:5,
+    losesWeek:2
+  },
+  {
+    name:"Παπαργυρόπουλος Νάσος",
+    image:"",
+    winsTotal:7,
+    losesTotal:9,
+    winsWeek:3,
+    losesWeek:4
+  },
+  {
+    name:"Ανδρονικίδου Ζωή",
+    image:"",
+    winsTotal:5,
+    losesTotal:3,
+    winsWeek:3,
+    losesWeek:1
+  },
+  {
+    name:"Πήχου Μαρίνα",
+    image:"",
+    winsTotal:5,
+    losesTotal:8,
+    winsWeek:2,
+    losesWeek:3
+  },
+  {
+    name:"Θεοδωρόπουλος Θοδωρής",
+    image:"",
+    winsTotal:5,
+    losesTotal:10,
+    winsWeek:1,
+    losesWeek:6
+  },
+  {
+    name:"Παρθένης Αλέξανδρος",
+    image:"",
+    winsTotal:5,
+    losesTotal:2,
+    winsWeek:0,
+    losesWeek:0
+  },
+  {
+    name:"Χατζίδου Ελένη",
+    image:"",
+    winsTotal:4,
+    losesTotal:4,
+    winsWeek:2,
+    losesWeek:1
+  },
+  {
+    name:"Δρυμωνάκος Γιάννης",
+    image:"",
+    winsTotal:4,
+    losesTotal:7,
+    winsWeek:3,
+    losesWeek:3
+  },
+  {
+    name:"Χαλικιά Κατερίνα",
+    image:"",
+    winsTotal:4,
+    losesTotal:10,
+    winsWeek:0,
+    losesWeek:5
+  },
+  {
+    name:"Σπυροπούλου Κωνσταντίνα",
+    image:"",
+    winsTotal:3,
+    losesTotal:2,
+    winsWeek:1,
+    losesWeek:2
+  },
+  {
+    name:"Θωμάς Νίκος",
+    image:"",
+    winsTotal:3,
+    losesTotal:4,
+    winsWeek:0,
+    losesWeek:2
+  },
+  {
+    name:"Καπαρού Ροδάνθη 	",
+    image:"",
+    winsTotal:3,
+    losesTotal:5,
+    winsWeek:1,
+    losesWeek:3
+  },
+  {
+    name:"Ενέν Ξένια",
+    image:"",
+    winsTotal:3,
+    losesTotal:8,
+    winsWeek:3,
+    losesWeek:3
+  },
+  {
+    name:"Τσεπάνης Κωνσταντίνος",
+    image:"",
+    winsTotal:3,
+    losesTotal:12,
+    winsWeek:2,
+    losesWeek:5
+  },
+  {
+    name:"Κοκκαλίτσα Αγγελική",
+    image:"",
+    winsTotal:1,
+    losesTotal:5,
+    winsWeek:1,
+    losesWeek:1
+  },
+
+
+
+
+]
